e367d144669c32077bd6165a6b4d4a05e9a1c6f9
gmarma23 <gmarma99@gmail.com>
2024-07-11 15:23:13 +0300

    Implemented client and server

---
+++ client.py
@@ -0,0 +1,76 @@
+import argparse
+import requests
+import json
+from colorama import Style, Fore
+
+from huffman import HuffmanCodec
+import crypto_utils
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str, required='true', help='Path to message file.')
+
+    return parser.parse_args()
+
+
+def read_message(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        content = ''.join(line.strip() for line in file.readlines())
+
+    return content
+
+
+def pretty_print_json(message: str, data: dict) -> None:
+    print(f'{Fore.GREEN}{message}:{Style.RESET_ALL}')
+    print(Fore.CYAN)
+    print(json.dumps(data, indent=4), end='\n\n')
+    print(Style.RESET_ALL)
+
+
+def send_payload(payload: dict, url: str='http://127.0.0.1:5000/') -> dict:
+    HEADERS = {'Content-Type': 'application/json'}
+
+    pretty_print_json('Sending payload', payload)
+
+    return requests.post(url, data=json.dumps(payload), headers=HEADERS).json()
+
+
+def main() -> None:
+    # Parse file path argument.
+    args = parse_args()
+
+    # Read message file
+    message = read_message(args.file)
+    
+    if len(message) == 0:
+        raise ValueError('Invalid message. Message should not be empty.')
+
+    # Calculate message hash.
+    sha256_hash = crypto_utils.sha256_hash(message)
+
+    # Calculate message entropy.
+    entropy = crypto_utils.entropy(message)
+
+    # Compress message.
+    encoded_message, huffman_dict = HuffmanCodec.encode(message)
+
+    # Send payload
+    payload = {
+        'encoded_message': encoded_message,
+        'compression_algorithm':'huffman',
+        'encoding': 'cyclic',
+        'parameters': [huffman_dict],
+        'errors': 0,
+        'SHA256': sha256_hash,
+        'entropy': entropy
+    }
+
+    response = send_payload(payload)
+
+    # Output server response.
+    pretty_print_json('Response from server', response)
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+
+from huffman import HuffmanCodec
+import crypto_utils
+
+
+def decompress(compression_algorithm: str, data: str, **kwargs) -> str:
+    match compression_algorithm:
+        case 'huffman':
+            if 'huffman_dict' not in kwargs:
+                raise TypeError('Missing parameter: huffman_dict')
+
+            return HuffmanCodec.decode(data, kwargs['huffman_dict'])
+        
+        case _:
+            raise ValueError(f'Unsupported compression algorithm: {compression_algorithm}')
+
+
+def decode(encoding: str, data: str, **kwargs) -> str:
+    match encoding:
+        case 'cyclic':
+            return data
+        
+        case _:
+            raise ValueError(f'Unsupported encoding: {encoding}')
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def receive_json():
+    # Retrieve data.
+    data = request.get_json()
+
+    # Decode message with proper algorithm.
+    decoded_message = decode(data['encoding'], data['encoded_message'])
+
+    # Decompress message with proper algorithm.
+    decoded_message = decompress(
+        data['compression_algorithm'], 
+        decoded_message,
+        huffman_dict = data['parameters'][0]
+    )
+
+    # Calculate new hash and compare with original.
+    decoded_sha256_hash = crypto_utils.sha256_hash(decoded_message)
+    sha256_diff = data['SHA256'] != decoded_sha256_hash
+
+    # Calculate new entropy and compare with original.
+    decoded_entropy = crypto_utils.entropy(decoded_message)
+    entropy_diff = data['entropy'] - decoded_entropy
+
+    # Send response
+    response = {
+        'decoded_message': decoded_message,
+        'error_corrections': 0,
+        'SHA256': decoded_sha256_hash,
+        'SHA256_diff': sha256_diff,
+        'entropy': decoded_entropy,
+        'entropy_diff': entropy_diff
+    }
+
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
